ed064fa5156e9ddcace5d3cae3f9874387a5b841
Tom <thomas.spencer@codurance.com>
2021-04-21 06:50:01 +0100

    add starter for project - yay!

---
+++ tests/Bowling/GameTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Katas\Bowling;
+
+use PHPUnit\Framework\TestCase;
+class GameTest extends TestCase
+{
+    public function testTest()
+    {
+        $this->fail('Hello!');
+    }
+}
